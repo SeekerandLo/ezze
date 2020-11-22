a9ea786103ada29f07f332b64dad838fb8ca4a28
liy <1037439896@qq.com>
2020-11-22 20:16:26 +0800

    feat(all): init

---
+++ bin/ezze.js
@@ -0,0 +1,9 @@
+const program = require('commander');
+const Ezze = require('../lib/ezze');
+
+program
+  .arguments('<query>')
+  .action((query) => {
+    Ezze.transform(query);
+  })
+  .parse(process.argv);
