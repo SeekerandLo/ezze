df3543f4ff146508f6621d017a463d46a3de8942
liy <1037439896@qq.com>
2020-12-13 12:47:54 +0800

    feat(transform): update exception handling.

--- bin/ezze.js
+++ bin/ezze.js
@@ -6,14 +6,14 @@ const Ezze = require('../lib/ezze');
 const ezzeClient = new Ezze();
 
 program
-  .version('0.0.7', '-v, --version')
+  .version('0.0.8', '-v, --version')
   .command('ezze <query>', 'translate words or sentences')
   .action((ezze) => {
     if (ezze.args.length === 0) {
       console.log(chalk.green('  ___ ___________ '))
       console.log(chalk.green(' / _ \\_  /_  / _ \\'))
       console.log(chalk.green('|  __// / / /  __/'))
-      console.log(chalk.green(' \\___/___/___\\___| v0.0.7'))
+      console.log(chalk.green(' \\___/___/___\\___| v0.0.8'))
     } else
       if (ezze.args.length > 1) {
         console.log(chalk.red('╮(╯-╰)╭, Please enter a word. You can wrap the content with a semicolon.'))
